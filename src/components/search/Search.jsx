ce4078718f3ce6e8297fe087daff75632d6a4745
sedrik124679 <sedrik124679@gmail.com>
2021-09-26 20:58:34 +0300

    config for search-panel

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,12 +1,17 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import {AlertContext} from "../../context/alertContext/AlertContext";
 
 const Search = () => {
-
+    const [value, setValue] = useState('')
     const {show} = useContext(AlertContext)
     const onSubmit = (event) => {
-        if(event.key === 'Enter'){
-            show('This is alert')
+        if(event.key !== 'Enter'){
+            return
+        }
+        if (value.trim()) {
+            console.log('make request to api', value)
+        } else {
+            show('Будь ласка, введіть дані користувача!')
         }
     }
 
@@ -15,7 +20,10 @@ const Search = () => {
             <input
                 type="text"
                 className='form-control'
-                placeholder='Enter nickname of user' onKeyPress={onSubmit}/>
+                placeholder='Enter nickname of user'
+                onKeyPress={onSubmit}
+                value={value}
+                onChange={(event) => setValue(event.target.value)}/>
         </div>
     );
 };
