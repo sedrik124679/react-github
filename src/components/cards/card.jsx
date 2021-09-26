3b62552ca4a288ecb511a6ed9bfe94f585f6d905
sedrik124679 <sedrik124679@gmail.com>
2021-09-26 19:47:20 +0300

    main page

---
+++ src/components/cards/card.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+
+const Card = () => {
+    return (
+        <div className='card'>
+            <img src={''} alt={''} className='card-img-top'/>
+            <div className="card-body">
+                <h5 className='card-title'>React JS</h5>
+                <Link to={'/profile/' + 'react'} className='btn btn-primary'>Open</Link>
+            </div>
+        </div>
+    );
+};
+
+export default Card;
